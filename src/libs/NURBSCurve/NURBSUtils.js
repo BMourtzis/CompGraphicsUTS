a5d2a8ccf056f32d7c54a9b497ec7a34e0514edd
Vasileios Mourtzis <vasilis.mourtzis@outlook.com>
2018-04-18 00:04:35 +1000

    fixed NURBSUtils

--- src/libs/NURBSCurve/NURBSUtils.js
+++ src/libs/NURBSCurve/NURBSUtils.js
@@ -1,40 +1,29 @@
-import {Vector3, Vector4} from "three";
+import { Vector3, Vector4 } from "three";
 
-// @author renej
 // NURBS utils
-// See NURBSCurve and NURBSSurface.
-
-// ************************************************************
-// NURBS Utils
-// ************************************************************
 
 let NURBSUtils = {
-
   // Finds knot vector span.
-  // p : degree
-  // u : parametric value
-  // U : knot vector
-  // returns the span
-  findSpan(p, u, U) {
-    let n = U.length - p - 1;
+  findSpan(degree, para, knot) {
+    let unit = knot.length - degree - 1;
 
-    if (u >= U[n]) {
-      return n - 1;
+    if (para >= knot[unit]) {
+      return unit - 1;
     }
 
-    if (u <= U[p]) {
-      return p;
+    if (para <= knot[degree]) {
+      return degree;
     }
 
-    let low = p;
-    let high = n;
+    let low = degree;
+    let high = unit;
     let mid = Math.floor((low + high) / 2);
 
-    while (u < U[mid] || u >= U[mid + 1]) {
-      if (u < U[mid]) {
+    while (para < knot[mid] || para >= knot[mid + 1]) {
+      if (para < knot[mid]) {
         high = mid;
       }
- else {
+      else {
         low = mid;
       }
 
@@ -43,37 +32,32 @@ let NURBSUtils = {
 
     return mid;
   },
-
   // Calculate basis functions. See The NURBS Book, page 70, algorithm A2.2
-  // span : span in which u lies
-  // u    : parametric point
-  // p    : degree
-  // U    : knot vector
   // returns array[p+1] with basis functions values.
-  calcBasisFunctions(span, u, p, U) {
-    let N = [];
+  calcBasisFunctions(span, para, degree, knot) {
+    let arr = [];
     let left = [];
     let right = [];
-    N[0] = 1.0;
+    arr[0] = 1.0;
 
-    for (let j = 1; j <= p; ++j) {
-      left[j] = u - U[span + 1 - j];
-      right[j] = U[span + j] - u;
+    for (let j = 1; j <= degree; ++j) {
+      left[j] = para - knot[span + 1 - j];
+      right[j] = knot[span + j] - para;
 
       let saved = 0.0;
 
-      for (let r = 0; r < j; ++r) {
-        let rv = right[r + 1];
-        let lv = left[j - r];
-        let temp = N[r] / (rv + lv);
-        N[r] = saved + (rv * temp);
+      for (let i = 0; i < j; ++i) {
+        let rv = right[i + 1];
+        let lv = left[j - i];
+        let temp = arr[i] / (rv + lv);
+        arr[i] = saved + (rv * temp);
         saved = lv * temp;
       }
 
-      N[j] = saved;
+      arr[j] = saved;
     }
 
-    return N;
+    return arr;
   },
 
   // Calculate B-Spline curve points. See The NURBS Book, page 82, algorithm A3.1.
@@ -82,22 +66,22 @@ let NURBSUtils = {
   // P : control points (x, y, z, w)
   // u : parametric point
   // returns point for given u
-  calcBSplinePoint(p, U, P, u) {
-    let span = this.findSpan(p, u, U);
-    let N = this.calcBasisFunctions(span, u, p, U);
-    let C = new Vector4(0, 0, 0, 0);
+  calcBSplinePoint(degree, knot, control, para) {
+    let span = this.findSpan(degree, para, knot);
+    let arr = this.calcBasisFunctions(span, para, degree, knot);
+    let vec = new Vector4(0, 0, 0, 0);
 
-    for (let j = 0; j <= p; ++j) {
-      let point = P[span - p + j];
-      let Nj = N[j];
+    for (let j = 0; j <= degree; ++j) {
+      let point = control[span - degree + j];
+      let Nj = arr[j];
       let wNj = point.w * Nj;
-      C.x += point.x * wNj;
-      C.y += point.y * wNj;
-      C.z += point.z * wNj;
-      C.w += point.w * Nj;
+      vec.x += point.x * wNj;
+      vec.y += point.y * wNj;
+      vec.z += point.z * wNj;
+      vec.w += point.w * Nj;
     }
 
-    return C;
+    return vec;
   },
 
   // Calculate basis functions derivatives. See The NURBS Book, page 72, algorithm A2.3.
@@ -107,19 +91,19 @@ let NURBSUtils = {
   // n    : number of derivatives to calculate
   // U    : knot vector
   // returns array[n+1][p+1] with basis functions derivatives
-  calcBasisFunctionDerivatives(span, u, p, n, U) {
+  calcBasisFunctionDerivatives(span, para, degree, deriv, knot) {
     let zeroArr = [];
-    for (let i = 0; i <= p; ++i) {
+    for (let i = 0; i <= degree; ++i) {
       zeroArr[i] = 0.0;
     }
 
     let ders = [];
-    for (let i = 0; i <= n; ++i) {
+    for (let i = 0; i <= deriv; ++i) {
       ders[i] = zeroArr.slice(0);
     }
 
     let ndu = [];
-    for (let i = 0; i <= p; ++i) {
+    for (let i = 0; i <= degree; ++i) {
       ndu[i] = zeroArr.slice(0);
     }
 
@@ -128,63 +112,63 @@ let NURBSUtils = {
     let left = zeroArr.slice(0);
     let right = zeroArr.slice(0);
 
-    for (let j = 1; j <= p; ++j) {
-      left[j] = u - U[span + 1 - j];
-      right[j] = U[span + j] - u;
+    for (let j = 1; j <= degree; ++j) {
+      left[j] = para - knot[span + 1 - j];
+      right[j] = knot[span + j] - para;
 
       let saved = 0.0;
 
-      for (let r = 0; r < j; ++r) {
-        let rv = right[r + 1];
-        let lv = left[j - r];
-        ndu[j][r] = rv + lv;
+      for (let i = 0; i < j; i++) {
+        let rv = right[i + 1];
+        let lv = left[j - i];
+        ndu[j][i] = rv + lv;
 
-        let temp = ndu[r][j - 1] / ndu[j][r];
-        ndu[r][j] = saved + (rv * temp);
+        let temp = ndu[i][j - 1] / ndu[j][i];
+        ndu[i][j] = saved + (rv * temp);
         saved = lv * temp;
       }
 
       ndu[j][j] = saved;
     }
 
-    for (let j = 0; j <= p; ++j) {
-      ders[0][j] = ndu[j][p];
+    for (let j = 0; j <= degree; ++j) {
+      ders[0][j] = ndu[j][degree];
     }
 
-    for (let r = 0; r <= p; ++r) {
+    for (let x = 0; x <= degree; x++) {
       let s1 = 0;
       let s2 = 1;
 
-      let a = [];
-      for (let i = 0; i <= p; ++i) {
-        a[i] = zeroArr.slice(0);
+      let z = [];
+      for (let i = 0; i <= degree; ++i) {
+        z[i] = zeroArr.slice(0);
       }
-      a[0][0] = 1.0;
+      z[0][0] = 1.0;
 
-      for (let k = 1; k <= n; ++k) {
-        let d = 0.0;
-        let rk = r - k;
-        let pk = p - k;
+      for (let y = 1; y <= deriv; y++) {
+        let w = 0.0;
+        let rk = x - y;
+        let pk = degree - y;
 
-        if (r >= k) {
-          a[s2][0] = a[s1][0] / ndu[pk + 1][rk];
-          d = a[s2][0] * ndu[rk][pk];
+        if (x >= y) {
+          z[s2][0] = z[s1][0] / ndu[pk + 1][rk];
+          w = z[s2][0] * ndu[rk][pk];
         }
 
         let j1 = rk >= -1 ? 1 : -rk;
-        let j2 = r - 1 <= pk ? k - 1 : p - r;
+        let j2 = x - 1 <= pk ? y - 1 : degree - x;
 
         for (let j = j1; j <= j2; ++j) {
-          a[s2][j] = (a[s1][j] - a[s1][j - 1]) / ndu[pk + 1][rk + j];
-          d += a[s2][j] * ndu[rk + j][pk];
+          z[s2][j] = (z[s1][j] - z[s1][j - 1]) / ndu[pk + 1][rk + j];
+          w += z[s2][j] * ndu[rk + j][pk];
         }
 
-        if (r <= pk) {
-          a[s2][k] = -a[s1][k - 1] / ndu[pk + 1][r];
-          d += a[s2][k] * ndu[r][pk];
+        if (x <= pk) {
+          z[s2][y] = -z[s1][y - 1] / ndu[pk + 1][x];
+          w += z[s2][y] * ndu[x][pk];
         }
 
-        ders[k][r] = d;
+        ders[y][x] = w;
 
         let j = s1;
         s1 = s2;
@@ -192,13 +176,13 @@ let NURBSUtils = {
       }
     }
 
-    let r = p;
+    let x = degree;
 
-    for (let k = 1; k <= n; ++k) {
-      for (let j = 0; j <= p; ++j) {
-        ders[k][j] *= r;
+    for (let i = 1; i <= deriv; i++) {
+      for (let j = 0; j <= degree; ++j) {
+        ders[i][j] *= x;
       }
-      r *= p - k;
+      x *= degree - i;
     }
 
     return ders;
@@ -211,16 +195,16 @@ let NURBSUtils = {
   // u  : Parametric points
   // nd : number of derivatives
   // returns array[d+1] with derivatives
-  calcBSplineDerivatives(p, U, P, u, nd) {
-    let du = nd < p ? nd : p;
+  calcBSplineDerivatives(degree, knot, control, para, nd) {
+    let du = nd < degree ? nd : degree;
     let CK = [];
-    let span = this.findSpan(p, u, U);
-    let nders = this.calcBasisFunctionDerivatives(span, u, p, du, U);
+    let span = this.findSpan(degree, para, knot);
+    let nders = this.calcBasisFunctionDerivatives(span, para, degree, du, knot);
     let Pw = [];
 
-    for (let i = 0; i < P.length; ++i) {
-      let point = P[i].clone();
-      // let w = point.w;
+    for (let i = 0; i < control.length; ++i) {
+      let point = control[i].clone();
+
       let {w} = point;
 
       point.x *= w;
@@ -230,18 +214,18 @@ let NURBSUtils = {
       Pw[i] = point;
     }
 
-    for (let k = 0; k <= du; ++k) {
-      let point = Pw[span - p].clone().multiplyScalar(nders[k][0]);
+    for (let i = 0; i <= du; i++) {
+      let point = Pw[span - degree].clone().multiplyScalar(nders[i][0]);
 
-      for (let j = 1; j <= p; ++j) {
-        point.add(Pw[span - p + j].clone().multiplyScalar(nders[k][j]));
+      for (let j = 1; j <= degree; j++) {
+        point.add(Pw[span - degree + j].clone().multiplyScalar(nders[i][j]));
       }
 
-      CK[k] = point;
+      CK[i] = point;
     }
 
-    for (let k = du + 1; k <= nd + 1; ++k) {
-      CK[k] = new Vector4(0, 0, 0);
+    for (let i = du + 1; i <= nd + 1; i++) {
+      CK[i] = new Vector4(0, 0, 0);
     }
 
     return CK;
@@ -249,10 +233,10 @@ let NURBSUtils = {
 
   // Calculate "K over I"
   // returns k!/(i!(k-i)!)
-  calcKoverI(k, i) {
+  calcKoverI(x, i) {
     let nom = 1;
 
-    for (let j = 2; j <= k; ++j) {
+    for (let j = 2; j <= x; ++j) {
       nom *= j;
     }
 
@@ -262,7 +246,7 @@ let NURBSUtils = {
       denom *= j;
     }
 
-    for (let j = 2; j <= k - i; ++j) {
+    for (let j = 2; j <= x - i; ++j) {
       denom *= j;
     }
 
@@ -285,14 +269,14 @@ let NURBSUtils = {
 
     let CK = [];
 
-    for (let k = 0; k < nd; ++k) {
-      let v = Aders[k].clone();
+    for (let j = 0; j < nd; j++) {
+      let vec = Aders[j].clone();
 
-      for (let i = 1; i <= k; ++i) {
-        v.sub(CK[k - i].clone().multiplyScalar(this.calcKoverI(k, i) * wders[i]));
+      for (let i = 1; i <= j; ++i) {
+        vec.sub(CK[j - i].clone().multiplyScalar(this.calcKoverI(j, i) * wders[i]));
       }
 
-      CK[k] = v.divideScalar(wders[0]);
+      CK[j] = vec.divideScalar(wders[0]);
     }
 
     return CK;
@@ -305,10 +289,10 @@ let NURBSUtils = {
   // u  : parametric points
   // nd : number of derivatives
   // returns array with derivatives.
-  calcNURBSDerivatives(p, U, P, u, nd) {
-    let Pders = this.calcBSplineDerivatives(p, U, P, u, nd);
+  calcNURBSDerivatives(degree, knot, control, para, nd) {
+    let Pders = this.calcBSplineDerivatives(degree, knot, control, para, nd);
 
-return this.calcRationalCurveDerivatives(Pders);
+    return this.calcRationalCurveDerivatives(Pders);
   },
 
   // Calculate rational B-Spline surface point. See The NURBS Book, page 134, algorithm A4.3.
@@ -317,34 +301,34 @@ return this.calcRationalCurveDerivatives(Pders);
   // P      : control points (x, y, z, w)
   // u, v   : parametric values
   // returns point for given (u, v)
-  calcSurfacePoint(p, q, U, V, P, u, v) {
-    let uspan = this.findSpan(p, u, U);
-    let vspan = this.findSpan(q, v, V);
-    let Nu = this.calcBasisFunctions(uspan, u, p, U);
-    let Nv = this.calcBasisFunctions(vspan, v, q, V);
+  calcSurfacePoint(p1, p2, U1, U2, control, para1, para2) {
+    let uspan = this.findSpan(p1, para1, U1);
+    let vspan = this.findSpan(p2, para2, U2);
+    let Nu = this.calcBasisFunctions(uspan, para1, p1, U1);
+    let Nv = this.calcBasisFunctions(vspan, para2, p2, U2);
     let temp = [];
 
-    for (let l = 0; l <= q; ++l) {
-      temp[l] = new Vector4(0, 0, 0, 0);
-      for (let k = 0; k <= p; ++k) {
-        let point = P[uspan - p + k][vspan - q + l].clone();
+    for (let i = 0; i <= p2; i++) {
+      temp[i] = new Vector4(0, 0, 0, 0);
+      for (let j = 0; j <= p1; j++) {
+        let point = control[uspan - p1 + j][vspan - p2 + i].clone();
         // let w = point.w;
         let {w} = point;
         point.x *= w;
         point.y *= w;
         point.z *= w;
-        temp[l].add(point.multiplyScalar(Nu[k]));
+        temp[i].add(point.multiplyScalar(Nu[j]));
       }
     }
 
     let Sw = new Vector4(0, 0, 0, 0);
-    for (let l = 0; l <= q; ++l) {
-      Sw.add(temp[l].multiplyScalar(Nv[l]));
+    for (let i = 0; i <= p2; i++) {
+      Sw.add(temp[i].multiplyScalar(Nv[i]));
     }
 
     Sw.divideScalar(Sw.w);
 
-return new Vector3(Sw.x, Sw.y, Sw.z);
+    return new Vector3(Sw.x, Sw.y, Sw.z);
   }
 };
 
