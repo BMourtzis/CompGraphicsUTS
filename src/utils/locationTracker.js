ff23597ca3b1c6c426d7b5d286cb144a1a9f6661
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2018-05-07 13:10:59 +1000

    Fix some code styles on locationTracker

--- src/utils/locationTracker.js
+++ src/utils/locationTracker.js
@@ -1,74 +1,63 @@
-/*
 //  PositionManager
 //
 //  this function is used to track the position of the player in the scene,
-//  we need to track the location of the player in case it is standing near a sculture 
-//  in that case we need to identify which platform and what ui we need to display 
+//  we need to track the location of the player in case it is standing near a sculture
+//  in that case we need to identify which platform and what ui we need to display
 //
-//  by nelson 
-*/
+//  by nelson
 
-import { Box3, Vector3 } from "three";
-import { scene, camera, engine, HUDRenderer } from "./engine";
-import { cowboy } from "../misc/cowboy";
-//Renderer settings used by the WebGLRenderer
-const rendererSettings = {
- precision: "lowp"
-};
+// import {Box3, Vector3} from "three";
+// import {scene, camera, engine, HUDRenderer} from "./engine";
+// import {cowboy} from "../misc/cowboy";
 
-
-function PositionManager(){
+function PositionManager() {
 
   // local var
-  var self = this;
+  let self = this;
 
-  var HUD_header = document.getElementById('HUD');
-  var HUD_info = document.getElementById('HUB_text');
+  let HUD_header = document.getElementById('HUD');
+  let HUD_info = document.getElementById('HUB_text');
 
-  var text_1 = "this is some text";
+  let text_1 = "this is some text";
 
-  var base_1;
+  let base_1;
 
-  this.init = function(){
-/*
-    let loader = new FBXLoader();
-    loader.load("models/cowboy.fbx", (obj) => {
-      console.log(cowboy);
-    });
-  */  
+  this.init = function() {
+    // let loader = new FBXLoader();
+    // loader.load("models/cowboy.fbx", (obj) => {
+    //   console.log(cowboy);
+    // });
+    //
     //console.log(cowboy.returnPosition());
-      
+
   }
 
 
-  // we get the player location from the playerCollider box 
-  // from the collider.js 
-  this.isPlayerOnPlatform = function(zPos, xPos){
-
-
-
+  // we get the player location from the playerCollider box
+  // from the collider.js
+  this.isPlayerOnPlatform = function(zPos, xPos) {
     self.updateUI(null);
+  };
 
-  }
+  this.updateUI = function(platformID) {
+    // check if we are currently in the game
+    // if we are the display the UI else dont show it
+    let element = document.body;
+    if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+      // show UI
+      HUD_header.innerHTML = text_1;
+      HUD_header.style.display = '';
+      HUD_info.style.display = '';
 
-  this.updateUI = function(platformID){
-      
-      // check if we are currently in the game 
-      // if we are the display the UI else dont show it 
-      var element = document.body;
-      if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
-          // show UI
-
-          HUD_header.innerHTML = text_1;
-          HUD_header.style.display = '';
-          HUD_info.style.display = '';
-
-      }else{
-          // hide UI
-          HUD_header.style.display = 'none';
-          HUD_info.style.display = 'none';
-      }
-  }
+    }
+    else {
+      // hide UI
+      HUD_header.style.display = 'none';
+      HUD_info.style.display = 'none';
+    }
+  };
 }
 
-export {PositionManager};
+export {
+  PositionManager
+};
