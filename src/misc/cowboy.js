19aa6a3af6ed8fc0836471e8afee9c94c9897fdd
Jack Cooper <pog777six@gmail.com>
2018-05-23 17:53:44 +1000

    Cowboy

--- src/misc/cowboy.js
+++ src/misc/cowboy.js
@@ -15,11 +15,10 @@ function cowboy() {
     obj.applyMatrix(matrix);
     ped.add(obj);
 
-<<<<<<< HEAD
     //Set right position and rotation
-    ped.position.set(-20, 1, 0);
-    obj.position.set(0, 11.4, 0);
-    obj.rotation.set(0, Math.degToRad(90), 0);
+      ped.position.set(-140, 1, 0);
+      obj.position.set(0, 11.4, 0);
+      obj.rotation.set(0, Math.degToRad(90), 0);
     addCollider(ped);
 
     let spotLight = addSpotlight(new Vector3(50, 40, 0));
@@ -27,47 +26,25 @@ function cowboy() {
 
     //Add a key binding toggle the light. Bidns the light to key "1"
     let lightID = addLightingHandler(49, spotLight);
-=======
-  let loader = new FBXLoader();
-  detailedPedestal().then((ped) => {
-    loader.load("models/cowboy.fbx", (obj) => {
-      // Scale the cowboy
-      let matrix = new Matrix4();
-      matrix.makeScale(0.01, 0.01, 0.01);
-      obj.applyMatrix(matrix);
-      ped.add(obj);
-
-      ped.position.set(-140, 1, 0);
-      obj.position.set(0, 11.4, 0);
-      obj.rotation.set(0, Math.degToRad(90), 0);
-      addCollider(ped);
-
-      let spotLight = new SpotLight(0xffffff, 0.5);
-      ped.add(spotLight);
-      spotLight.intensity = 0;
-
-      spotLight.position.set(50, 40, 0);
-      spotLight.rotation.set(0, Math.degToRad(0), 0);
->>>>>>> Jack-RoomLayout
 
     wallSwitch(new Vector3(0, 8, 43), lightID);
 
-      let text = "TEST, right now you are looking at the cowboy";
-      addPointerTrigger(ped, text, lookCallback, clickCallback);
-      // engine.outlineObject(ped);
+    // engine.outlineObject(ped);
 
     // add Y rotation to the model
     addYRotation(obj);
 
-      addLightingHandler(49, spotLight);
+    addPointerTrigger(ped, "this is a test", lookCallback, clickCallback);
 
-      addTrigger(50, ped.position, () => {
-        spotLight.intensity = 1;
-      }, 0);
+    //Trigger to turn the light on when entering the sphere
+    addTrigger(50, ped.position, () => {
+      spotLight.intensity = 1;
+    }, 0);
 
-      addTrigger(50, ped.position, () => {
-        spotLight.intensity = 0;
-      }, 1);
+    //Trigger to turn the light off when leaving the sphere
+    addTrigger(50, ped.position, () => {
+      spotLight.intensity = 0;
+    }, 1);
 
     scene.add(ped);
   }, (err) => {
